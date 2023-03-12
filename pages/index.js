7995c76afb085d1475f5b24b7953fea43627da4f
ReturnerNaGithub <damsan1@gmail.com>
2023-03-12 15:17:38 +0100

    drugi commin

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Link from 'next/link';
+
 import { Footer, Navbar } from '../components';
-import { Games, Explore, Feedback, GetStarted, Logo, WhatsNew, } from '../sections';
+import { Games, Explore, Feedback, GetStarted, Logo, WhatsNew } from '../sections';
 
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
@@ -12,7 +12,6 @@ const Home = () => (
     <GetStarted />
     <Feedback />
     <Footer />
-    <Link href="/Rig">Rig<div><h1>Rig</h1></div></Link>
   </div>
 );
 
