62a377a66f1c4083a0070d545fe608d3601b5d88
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2023-07-18 10:58:34 +0100

    Update InsightCard.jsx

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 import { fadeIn } from '../utils/motion';
 
-const InsightCard = ({ imgUrl, title, subtitle, link,  index }) => (
+const InsightCard = ({ imgUrl, title, subtitle, link, index }) => (
   <motion.div
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
