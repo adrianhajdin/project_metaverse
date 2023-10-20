c88cb3ded07047ef69ce95b14faacc8b8b5d2b67
ssssbssss <sbhan1198@naver.com>
2023-10-20 22:11:57 +0900

    explore card 사진 변경

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => (
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[50px] text-[44px] text-white">
+        <h4 className="font-bold md:text-[50px] text-[44px] text-black">
            주식을 누구에게나 쉽게
         </h4>
         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
@@ -35,10 +35,10 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-normal text-[18px] text-white">
+          <h4 className="font-normal text-[18px] text-black">
             금요주식회
           </h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
+          <p className="font-normal text-[14px] text-black opacity-50">
             Copyright © 2023 금요주식회 . All rights reserved.
           </p>
 
