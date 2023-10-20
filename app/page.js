0a60511fb2141c6e4c17a0d113bfea42b19af4b4
ssssbssss <sbhan1198@naver.com>
2023-10-20 21:42:58 +0900

    배경화면 변경

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className="bg-primary-black overflow-hidden">
+  <div className="bg-white overflow-hidden">
     <Navbar />
     <Hero />
     <div className="relative">
