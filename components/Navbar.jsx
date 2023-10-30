6e42cf6178c37801efa58c223bf4f65e7a638baa
ssssbssss <sbhan1198@naver.com>
2023-10-30 20:53:15 +0900

    동적 검색창 구현

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [searchHiddenBar, setSearchHiddenBar] = useState(true);
+
+  const handleSearch = () => {
+    setSearchHiddenBar(searchHiddenBar => !searchHiddenBar);
+  };
+
+  return(
   <motion.nav
     variants={navVariants}
     initial="hidden"
@@ -16,21 +25,59 @@ const Navbar = () => (
     <div
       className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
     >
-      <img
-        src="/search.svg"
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-black">
-        금요주식회
-      </h2>
+      <NavRightLayout>
+        {searchHiddenBar ? (
+              <img src="/search.svg"
+              alt="search"
+              className="w-[24px] h-[24px] object-contain" onClick={handleSearch}/>
+            ) : (
+              <SearchBar>
+                <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" onClick={handleSearch} />
+                <SearchBarInput />
+              </SearchBar>
+          )}
+      </NavRightLayout>
+      
+      <NavMiddleLayout>
+        <h2 className="font-extrabold text-[24px] leading-[30.24px] text-black">
+          금요주식회
+        </h2>
+      </NavMiddleLayout>
       <img
         src="/menu.svg"
         alt="menu"
         className="w-[24px] h-[24px] object-contain"
-      />
+      />  
     </div>
   </motion.nav>
-);
+  )
+};
+
+const NavRightLayout = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+const NavMiddleLayout = styled.div`
+  display: flex;
+  position: fixed;
+  left: 700px;
+`;
+
+const SearchBar = styled.div`
+  position: relative;
+  width: 200px;
+  height: 22px;
+`;
+
+const SearchBarInput = styled.input`
+  position: absolute;
+  top : -5px;
+  left : -20px;
+  height: 32px;
+  margin: 2px 0 0 59px;
+  background-color:transparent;
+  border: 2px solid white;
+`;
 
 export default Navbar;
